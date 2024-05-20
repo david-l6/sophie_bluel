5dc2bff9fc7a45dbc431cb6a2d42f50aea87e650
david-l6 <david.l6@proton.me>
2024-05-20 17:41:31 +0200

    Envoi d'un nouveau work

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -71,8 +71,9 @@ photo.addEventListener('click', async (event) => {
                 'Authorization': `Bearer ${localStorage.getItem('authToken')}`
             }
         });
-        if (response.status === 200) {
+        if (response.status === 200 || 204) {
             figure.remove();
+            getWorks();
         }
     }
 });
@@ -91,6 +92,8 @@ btn.addEventListener('click', () => {
 arrow.addEventListener('click', () => {
     modal1.style.display = 'block';
     modal2.style.display = 'none';
+    photoChoisi.style.display = 'none';
+    ajoutPhoto.style.display = 'flex';
 });
 
 // Chargement des catégorie dans le formulaire
@@ -115,11 +118,57 @@ renderCategories();
 
 // Affichage de la photo selectionné depuis mon ordinateur dans la div .photo_choisi
 const input = document.querySelector('input[type="file"]');
-const photoChoisi = document.querySelector('.photo_choisi');
+const photoChoisi = document.querySelector('.photo_choisie');
+const ajoutPhoto = document.querySelector('.ajout_photo');
 input.addEventListener('change', () => {
+    const file = input.files[0];
     const reader = new FileReader();
     reader.onload = () => {
-        photoChoisi.innerHTML = `<img src="${reader.result}" alt="photo_choisi">`;
+        photoChoisi.style.display = 'block';
+        photoChoisi.innerHTML = `<img src="${reader.result}" alt="photo">`;
+        ajoutPhoto.style.display = 'none';
     };
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(file);
 });
+
+// Si tout les champs du formulaire sont remplis, alors activé le bouton "Valider"
+const form = document.querySelector('form');
+const btnValider = document.querySelector('#valider');
+const titre = document.querySelector('#titre');
+const categorie = document.querySelector('#categorie');
+const photoAjoute = document.querySelector('#photo');
+btnValider.disabled = true;
+form.addEventListener('input', () => {
+    if (titre.value !== "" && categorie.value > 0 && photoAjoute.value !== "") {
+        btnValider.disabled = false;
+        btnValider.classList.add('bouton_actif');
+    } else {
+        btnValider.disabled = true;
+        btnValider.classList.remove('bouton_actif');
+    }
+});
+
+// Envoie du formulaire pour ajouter une photo lors du click sur le bouton "Valider"
+btnValider.addEventListener('click', async (event) => {
+    event.preventDefault();
+    let data = new FormData();
+    data.append('image', input.files[0]);
+    data.append('title', titre.value);
+    data.append('category', categorie.value);
+    
+    fetch('http://localhost:5678/api/works', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+        },
+        body: data
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
+
+// Mettre un message dans la console une fois que le projet a bien été envoyé
+// Réinitialisé la modal2 pour permettre d'envoyer un nouveau projet.
